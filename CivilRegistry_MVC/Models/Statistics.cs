d3f2c10fb20d300b3b75f62e36a355923958daac
Granit Krasniqi <granit.krasniqi1@riinvest.net>
2020-12-08 02:31:11 +0100

    Editing

--- CivilRegistry_MVC/Models/Statistics.cs
+++ CivilRegistry_MVC/Models/Statistics.cs
@@ -14,95 +14,32 @@ namespace CivilRegistry_MVC.Models
 {
     public class Statistics
     {
+        private static CivilRegistryModel db = new CivilRegistryModel();
+
         public static int BirthNumber()
         {
-            using (SqlConnection conn = DbHelper.GetConnection())
-            {
-                using (SqlCommand command = DbHelper.Command(conn, "usp_BirthNumber", CommandType.StoredProcedure))
-                //using (SqlCommand command = DbHelper.Command(conn, "select count(PersonID) as number from People", CommandType.Text))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return int.Parse(reader["number"].ToString());
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                }
-            }
+            return db.People.Count();
         }
 
         public static int DeathNumber()
         {
-            using (SqlConnection conn = DbHelper.GetConnection())
-            {
-                using (SqlCommand command = DbHelper.Command(conn, "usp_DeathNumber", CommandType.StoredProcedure))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return int.Parse(reader["number"].ToString());
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                }
-            }
+            return db.DeathCertificates.Count();
         }
 
         public static int MarriageNumber()
         {
-            using (SqlConnection conn = DbHelper.GetConnection())
-            {
-                using (SqlCommand command = DbHelper.Command(conn, "usp_MarriageNumber", CommandType.StoredProcedure))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return int.Parse(reader["number"].ToString());
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                }
-            }
+            return db.MarriageCertificates.Count();
         }
 
         public static int MigrationNumber()
         {
-            using (SqlConnection conn = DbHelper.GetConnection())
-            {
-                using (SqlCommand command = DbHelper.Command(conn, "usp_MigrationNumber", CommandType.StoredProcedure))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return int.Parse(reader["number"].ToString());
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                }
-            }
+            return db.Passports.Count();
         }
         public static int CalculateAge(DateTime Birthday)
         {
             int age = DateTime.Now.Year - Birthday.Year;
             if (DateTime.Now.DayOfYear < Birthday.DayOfYear)
-            { age = age - 1; }
+            { age -= 1; }
             return age;
         }
 
